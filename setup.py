e40f02b7d8c2b790f316818e7f874c793b02253b
William Pettersson <william.pettersson@gmail.com>
2017-04-02 10:07:49 +1000

    Python3 compatibility in setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from distutils.util import convert_path
 
-execfile('lib/xapers/version.py')
+version_path = convert_path('lib/xapers/version.py')
+version_namespace = {}
+with open(version_path) as version_file:
+    exec(version_file.read(), version_namespace)
 
 setup(
     name = 'xapers',
-    version = __version__,
+    version = version_namespace['__version__'],
     description = 'Xapian article indexing system.',
     author = 'Jameson Rollins',
     author_email = 'jrollins@finestructure.net',
